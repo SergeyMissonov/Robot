8404224abb3474bc68ebb1c9654b9b31c3775444
Sergey Missonov <missonov@mail.ru>
2024-04-02 10:27:57 +0400

    изменено

--- rosbots_description/scripts/fr2.sh
+++ rosbots_description/scripts/fr2.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-ssh sergey-X55A@192.168.1.69
+# Установливаем SSH-сервер
+sudo apt-get install openssh-server
+# Копируем публичный ключ на удаленный сервер
+ssh-copy-id sergey@127.0.0.1
+# Подключаемся к роботу 
+ssh sergey@127.0.0.1
